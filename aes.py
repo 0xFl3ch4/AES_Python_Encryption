4732f232abfa2c2a2e3407b0c45319e86e87e4c0
flecha418 <ricardoferreiraclf@gmail.com>
2023-05-29 14:51:16 +0100

    changes in aes code

--- aes.py
+++ aes.py
@@ -1,67 +1,43 @@
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 
-key = get_random_bytes(32) # Use a stored / generated key
-file_to_encrypt = 'ficheiro.txt'
-buffer_size = 65536 # 64kb
+def encrypt_file(key, input_file, output_file):
+    cipher = AES.new(key, AES.MODE_CBC)
+    iv = cipher.iv
 
-# === Encrypt ===
+    with open(input_file, 'rb') as file:
+        plaintext = file.read()
 
-# Open the input and output files
-input_file = open(file_to_encrypt, 'rb')
-output_file = open(file_to_encrypt + '.encrypted', 'wb')
+    ciphertext = cipher.encrypt(pad(plaintext, AES.block_size))
 
-# Create the cipher object and encrypt the data
-cipher_encrypt = AES.new(key, AES.MODE_CBC)
+    with open(output_file, 'wb') as file:
+        file.write(iv)
+        file.write(ciphertext)
 
-# Initially write the iv to the output file
-output_file.write(cipher_encrypt.iv)
+def decrypt_file(key, input_file, output_file):
+    with open(input_file, 'rb') as file:
+        iv = file.read(16)
+        ciphertext = file.read()
 
-# Keep reading the file into the buffer, encrypting then writing to the new file
-buffer = input_file.read(buffer_size)
-while len(buffer) > 0:
-    ciphered_bytes = cipher_encrypt.encrypt(buffer)
-    output_file.write(ciphered_bytes)
-    buffer = input_file.read(buffer_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    decrypted_data = unpad(cipher.decrypt(ciphertext), AES.block_size)
 
-# Close the input and output files
-input_file.close()
-output_file.close()
+    with open(output_file, 'wb') as file:
+        file.write(decrypted_data)
 
-# === Decrypt ===
+# Get user input
+operation = input("Enter 'E' for encryption or 'D' for decryption: ").upper()
+input_file = input("Enter the name of the input file: ")
+output_file = input("Enter the name of the output file: ")
+key = input("Enter the encryption key (16 bytes): ").encode()
 
-# Open the input and output files
-input_file = open(file_to_encrypt + '.encrypted', 'rb')
-output_file = open(file_to_encrypt + '.decrypted', 'wb')
-
-# Read in the iv
-iv = input_file.read(16)
-
-# Create the cipher object and encrypt the data
-cipher_encrypt = AES.new(key, AES.MODE_CBC, iv=iv)
-
-# Keep reading the file into the buffer, decrypting then writing to the new file
-buffer = input_file.read(buffer_size)
-while len(buffer) > 0:
-    decrypted_bytes = cipher_encrypt.decrypt(buffer)
-    output_file.write(decrypted_bytes)
-    buffer = input_file.read(buffer_size)
-
-# Close the input and output files
-input_file.close()
-output_file.close()
-
-# === Proving the data matches (hash the files and compare the hashes) ===
-import hashlib
-
-def get_file_hash(file_path):
-    block_size = 65536
-    file_hash = hashlib.sha256()
-    with open(file_path, 'rb') as f:
-        fb = f.read(block_size)
-        while len(fb) > 0:
-            file_hash.update(fb)
-            fb = f.read(block_size)
-    return file_hash.hexdigest()
-
-assert get_file_hash(file_to_encrypt) == get_file_hash(file_to_encrypt + '.decrypted'), 'Files are not identical'
+# Perform encryption or decryption based on user input
+if operation == 'E':
+    encrypt_file(key, input_file, output_file)
+    print('File encrypted successfully.')
+elif operation == 'D':
+    decrypt_file(key, input_file, output_file)
+    print('File decrypted successfully.')
+else:
+    print('Invalid operation. Please choose either "E" or "D".')
