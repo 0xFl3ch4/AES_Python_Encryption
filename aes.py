4f507f3edc4834d9b9b14ee2dfc99c241236bef2
flecha418 <ricardoferreiraclf@gmail.com>
2023-05-31 11:46:39 +0100

    Decryption

--- aes.py
+++ aes.py
@@ -26,6 +26,26 @@ def encrypt(key, filename):
 
                 outfile.write(encryptor.encrypt(chunk))
 
+def decrypt(key, filename):
+    chunksize = 64*1024
+    outputFile = filename[11:]
+
+    with open(filename, 'rb') as infile:
+        filesize = int(infile.read(16))
+        IV = infile.read(16)
+
+        decryptor= AES.new(key, AES.MODE_CBC, IV)
+
+        with open(outputFile, 'wb') as outfile:
+            while True:
+                chunk = infile.read(chunksize)
+
+                if len(chunk) == 0:
+                    break
+
+                outfile.write(decryptor.decrypt(chunk))
+
+            outfile.truncate(filesize)
 
 def getKey(password):
     hasher = SHA256.new(password.encode('utf-8'))
